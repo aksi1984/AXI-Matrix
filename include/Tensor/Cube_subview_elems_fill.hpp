a09ccea0a4a7d50967eb41d1b1765b7a1e0402bb
Pawel Aksman <pablo_xmen@op.pl>
2020-05-06 21:36:43 +0200

    removed the get() function with the initializer list as a argument

--- include/Tensor/Cube_subview_elems_fill.hpp
+++ include/Tensor/Cube_subview_elems_fill.hpp
@@ -7,25 +7,15 @@
 namespace linalg
 {
 
+template<typename T>
+struct gett
+{
+    using begin = typename T::value_type;
+    using type = typename begin::value_type;
+};
+
     struct Row
     {
-        template<typename MatType>
-        static Matrix<typename MatType::value_type> get(MatType src, std::size_t row)
-        {
-            using value_type = typename MatType::value_type;
-
-            std::size_t rows = 1;
-            std::size_t cols = src.cols();
-
-            Matrix<value_type> dest(rows, cols);
-
-            for(auto src_iter = src.begin_row(row), dest_iter = dest.begin_row(0); src_iter != src.end_row(row); ++src_iter, ++ dest_iter)
-            {
-                *dest_iter = *src_iter;
-            }
-
-            return dest;
-        }
 
         template<typename MatType>
         static Matrix<typename MatType::value_type> get(MatType src, std::size_t first_row, std::size_t last_row)
@@ -49,77 +39,20 @@ namespace linalg
         }
 
         template<typename MatType>
-        static Matrix<typename MatType::value_type> get(MatType src, std::initializer_list<std::size_t> indices)
+        static void fill(MatType& mat, std::size_t first_row, std::size_t last_row, typename MatType::value_type value)
         {
-            using value_type = typename MatType::value_type;
-
-            std::size_t rows = indices.size();
-            std::size_t cols = src.cols();
-
-            Matrix<value_type> dest(rows, cols);
-            std::size_t dest_row_count = 0;
-
-            for(auto list_iter = indices.begin(); list_iter != indices.end(); ++list_iter, ++dest_row_count)
+            for(std::size_t i = first_row, dest_row_count = 0; i < last_row; ++i, ++dest_row_count)
             {
-                for(auto src_iter = src.begin_row(*list_iter), dest_iter = dest.begin_row(dest_row_count); src_iter != src.end_row(*list_iter); ++src_iter, ++ dest_iter)
+                for(auto row_iter = mat.begin_row(i); row_iter != mat.end_row(i); ++row_iter)
                 {
-                    *dest_iter = *src_iter;
+                    *row_iter = value;
                 }
-            }
-
-            return dest;
-        }
-
-        template<typename MatType>
-        static void set(MatType src, MatType dest, std::size_t row)
-        {
-            for(auto src_iter = src.begin_row(row), dest_iter = dest.begin_row(row); src_iter != src.end_row(row); ++src_iter, ++ dest_iter)
-            {
-                *dest_iter = *src_iter;
-            }
-        }
-
-        template<typename MatType>
-        static void set(MatType src, MatType& dest, std::initializer_list<std::size_t> indices)
-        {
-            for(auto list_iter = indices.begin(); list_iter != indices.end(); ++list_iter)
-            {
-                for(auto src_iter = src.begin_row(*list_iter), dest_iter = dest.begin_row(*list_iter); src_iter != src.end_row(*list_iter); ++src_iter, ++ dest_iter)
-                {
-                    *dest_iter = *src_iter;
-                }
-            }
-        }
-
-        template<typename MatType>
-        static void fill(MatType& dest, std::size_t n, typename MatType::value_type value)
-        {
-            for(auto dest_iter = dest.begin_row(n); dest_iter != dest.end_row(n); ++dest_iter)
-            {
-                *dest_iter = value;
             }
         }
     };
 
     struct Col
     {
-        template<typename MatType>
-        static Matrix<typename MatType::value_type> get(MatType src, std::size_t nth_col)
-        {
-            using value_type = typename MatType::value_type;
-
-            std::size_t rows = src.rows();
-            std::size_t cols = 1;
-
-            Matrix<value_type> dest(rows, cols);
-
-            for(auto src_iter = src.begin_col(nth_col), dest_iter = dest.begin_col(0); src_iter != src.end_col(nth_col); ++src_iter, ++ dest_iter)
-            {
-                *dest_iter = *src_iter;
-            }
-
-            return dest;
-        }
 
         template<typename MatType>
         static Matrix<typename MatType::value_type> get(MatType src, std::size_t first_col, std::size_t last_col)
@@ -142,27 +75,6 @@ namespace linalg
             return dest;
         }
 
-        template<typename MatType>
-        static Matrix<typename MatType::value_type> get(MatType src, std::initializer_list<std::size_t> indices)
-        {
-            using value_type = typename MatType::value_type;
-
-            std::size_t rows = src.rows();
-            std::size_t cols = indices.size();
-
-            Matrix<value_type> dest(rows, cols);
-            std::size_t dest_col_count = 0;
-
-            for(auto list_iter = indices.begin(); list_iter != indices.end(); ++list_iter, ++dest_col_count)
-            {
-                for(auto src_iter = src.begin_col(*list_iter), dest_iter = dest.begin_col(dest_col_count); src_iter != src.end_col(*list_iter); ++src_iter, ++ dest_iter)
-                {
-                    *dest_iter = *src_iter;
-                }
-            }
-
-            return dest;
-        }
     };
 
 } // namespace linalg
